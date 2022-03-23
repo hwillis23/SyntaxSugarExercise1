5926f4cbb093b3efbf13199540815f0268cab683
Hannah Willis <hwillis8817@gmail.com>
2022-03-23 13:52:01 -0500

    init complete syntexs exercise

---
+++ SyntaxSugarExercise1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SyntaxSugarExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater than or equal to nine";
+
+            Console.WriteLine(response);
+
+            
+                //Inferred Typing
+               //String Interpolation
+               //and the Ternary Operator
+        }
+    }
+}
